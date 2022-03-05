bf4e1a80980609391041cc485b1be383ae0b1b82
martin-rsg <martinrosenberg98@gmail.com>
2022-03-05 16:45:18 -0500

    Added orders.json file

--- Production_Planner.py
+++ Production_Planner.py
@@ -1,7 +1,6 @@
-
-
 import evo
 import orderbook
+import json
 import random as rnd
 import pprint as pp
 
@@ -9,9 +8,9 @@ import pprint as pp
 
 Solution data structure:
 
-Each solution is a list of unique order id's:
+Each solution is a tuple of unique order id's:
 
-[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], orderBook)
 
 ---> our goal is to place these in the best possible order
  
@@ -51,69 +50,88 @@ def setups(l, orderBook):
         tuples: [(X, Z), (Z, A), (A, A), (A, B), etc...]
         setups: [1,       1,             1,      ....].sum() = 3
         
-        
-        
-        
-        
+
         
 Scoring func: lowPriority
 
-
-
-
 Scoring func: delays
-        
-
-
 """
 
 
+def setups(t):
 
+    l, orderBook = t[0], t[1]
+    print(l)
 
-def setups(input):
-    pass
+    codes = [orderBook[order]['product_code'] for order in l]
+    return sum([1 for t in [(codes[i], codes[i + 1]) for i in range(len(codes) - 1)] if t[0] != t[1]])
+
 
 def lowPriority(input):
     pass
 
+
 def delays(input):
     pass
 
+
 def swapper(solutions):
     """ Agent: Swap two random values """
-    L = solutions[0]
+    L = solutions[0][0]
     i = rnd.randrange(0, len(L))
     j = rnd.randrange(0, len(L))
     L[i], L[j] = L[j], L[i]
-    return L
+    return (L, solutions[0][1])
 
 
 def main():
-
     # Create environment
     E = evo.Evo()
 
+    # Create the initial order book
+    with open('orders.json') as json_file:
+        order_data = json.load(json_file)
+
+    """ NOTE: This framework uses an orderbook object to simulate that possibility of adding and 
+              removing orders, as they would be at a real life factory. This makes the code more generalized,
+              and more useful.
+    """
     O = orderbook.Orderbook()
-    O.add_order(1, 50, "Z")
-    O.add_order(0, 20, "X")
-    pp.pprint(O.orderBook)
+
+    for k, v in order_data.items():
+
+        if v['priority'] == "HIGH":
+            priority = 1
+        else:
+            priority = 0
+
+        order_quantity = v['quantity']
+        product_code = v['product']
+
+        O.add_order(priority, order_quantity, product_code)
+
+
+
+    # pp.pprint(O.orderBook)
 
     # Register fitness criteria
     E.add_fitness_criteria("setups", setups)
-    E.add_fitness_criteria("lowPriority", lowPriority)
-    E.add_fitness_criteria("delays", delays)
+    # E.add_fitness_criteria("lowPriority", lowPriority)
+    # E.add_fitness_criteria("delays", delays)
 
     # Register agents
+
     E.add_agent("swapper", swapper, 1)
 
     # Add initial solution
-    # L = [rnd.randrange(1, 99) for _ in range(20)]
-    # E.add_solution(L)
+    T = (list(O.orderBook.keys()), O.orderBook)
+    E.add_solution(T)
     # print(E)
 
     # Run the evolver
-    # E.evolve(100000, 500, 10000)
+    E.evolve(100000, 500, 10000)
 
+    print(setups(T))
 
 if __name__ == '__main__':
     main()
--- orderbook.py
+++ orderbook.py
@@ -6,7 +6,7 @@ class Orderbook:
 
     def __init__(self):
         self.orderBook = {}
-        self.order_number = 0
+        self.order_number = 1
 
     def add_order(self, priority, order_quantity, product_code):
 
@@ -15,7 +15,13 @@ class Orderbook:
                                                            'product_code': product_code}
         self.order_number += 1
 
+    def add_orders_from_json(self, json):
+        pass
+
+
+
     def remove_order(self, order_number):
 
         self.orderBook.remove(order_number)
+
 

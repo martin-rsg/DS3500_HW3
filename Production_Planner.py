62d576c40c04967e15a5c2577f41c370487f5db9
martin-rsg <martinrosenberg98@gmail.com>
2022-03-11 17:07:34 -0800

    Added setupsAgent and priorityAgent, both working order

--- Production_Planner.py
+++ Production_Planner.py
@@ -63,7 +63,6 @@ Scoring func: delays
 
 
 def setups(t):
-
     l, orderBook = t[0], t[1]
     codes = [orderBook[order]['product_code'] for order in l]
     result = sum([1 for t in [(codes[i], codes[i + 1]) for i in range(len(codes) - 1)] if t[0] != t[1]])
@@ -71,7 +70,6 @@ def setups(t):
 
 
 def lowPriority(t):
-
     """ Go through order list until hitting final high priority order
 
         Add to counter of quantities"""
@@ -82,12 +80,12 @@ def lowPriority(t):
     # print(priorityValues)
 
     return sum([orderBook[orderNum]['order_quantity']
-                for orderNum in l
-                if not priorityValues[orderNum-1] and not sum(priorityValues[orderNum:]) == 0])
+                    for orderNum in l
+                        if not priorityValues[orderNum - 1]
+                        and not sum(priorityValues[orderNum:]) == 0])
 
 
 def delays(t):
-
     """ For each order in t[0]      <-----  [6,2,3,89,4,5,7]
             check if t[0][i] > t[0][i+1]
             then counter += orderBook[t[0][quantity]]
@@ -101,13 +99,13 @@ def delays(t):
 
     for i in range(len(l))[1:]:
         curr_order = l[i]
-        prev_order = l[i-1]
+        prev_order = l[i - 1]
 
 
         if curr_order < prev_order:
             counter += orderBook[curr_order]['order_quantity']
 
-    return sum([orderBook[l[i]]['order_quantity'] for i in range(len(l))[1:] if l[i] < l[i-1]])
+    return sum([orderBook[l[i]]['order_quantity'] for i in range(len(l))[1:] if l[i] < l[i - 1]])
 
 
 def swapper(solutions):
@@ -119,8 +117,79 @@ def swapper(solutions):
     return (L, solutions[0][1])
 
 
-def agent1(solutions):
-    """ Agent: """
+def setupsAgent(solutions):
+    """ Finds groupings of like products, searching for groups of 3.
+        Picks a group at random, and selects that product.
+        Finds that product eleswhere in the production schedule (that isn't in a block).
+        Moves it to the selected block."""
+
+    l, orderBook = solutions[0][0], solutions[0][1]
+
+    # Find blocks of like products, search for blocks of 3+
+    products = [orderBook[orderNum]['product_code'] for orderNum in l]
+    blockLocations = [i for i in range(len(products)-3) if products[i] == products[i+1] == products[i+2]]
+
+    # pick a random block to focus on, and get which product that is
+    focusBlockLocation = rnd.choice(blockLocations)
+    focusProduct = products[focusBlockLocation]
+
+    # get list of other indexes at which that product exists
+    focusProductLocations = [i for i in range(len(products)) if products[i] == focusProduct]
+
+    # disqualify locations that are within a block
+    popList = []
+    for i in range(3,len(focusProductLocations)):
+        cond1 = focusProductLocations[i - 2] in blockLocations
+        cond2 = focusProductLocations[i - 1] in blockLocations
+        cond3 = focusProductLocations[i - 0] in blockLocations
+
+        if cond1 or cond2 or cond3:
+            popList.append(i)
+
+    for location in sorted(popList, reverse=True):
+        del focusProductLocations[location]
+
+    # pick an element from focusProductLocations at random and insert it at the focusBlockLocation
+    focusProduct_i = rnd.choice(focusProductLocations)
+    l.insert(focusBlockLocation, l[focusProduct_i])
+
+    # remove that element from the list
+    if focusProduct_i < focusBlockLocation:
+        l.pop(focusProduct_i)
+    else:
+        l.pop(focusProduct_i+1)
+
+    return (l, solutions[0][1])
+
+def priorityAgent(solutions):
+    """ Agent directed at placing low priority agents after the final high priority order
+        basic mechanism is to take a random low priority order that is before last high priority order,
+        and place it into a random spot after the last high priority order"""
+
+    l, orderBook = solutions[0][0], solutions[0][1]
+
+    priorityValues = [orderBook[orderNum]['priority'] for orderNum in l]
+
+    # loop through priority values backwards to find index value of last high priority order
+    for i in reversed(range(len(l))):
+        if l[i]:
+            lastPriorityIndex = i
+            break
+
+    # list of low priority index values for l, before the last high priority order
+    lowPriorityIndexValues = [idx for idx in range(len(priorityValues)) if priorityValues[idx] == 0][:lastPriorityIndex]
+
+    # select one at random
+    i_LowPriority = lowPriorityIndexValues[rnd.randrange(0, len(lowPriorityIndexValues))]
+
+    # select a random spot after last high priority order
+    i_AfterHighPriority = rnd.randrange(lastPriorityIndex, len(l)) + 1
+
+    # insert low priority order after last high priority
+    l.insert(i_AfterHighPriority, l[i_LowPriority])
+    l.pop(i_LowPriority)
+
+    return (l, solutions[0][1])
 
 def collection(pop):
     """ collects all the data and creates a data frame
@@ -192,7 +261,9 @@ def main():
 
     # Register agents
 
-    E.add_agent("swapper", swapper, 1)
+    E.add_agent("swapper", swapper, 3)
+    E.add_agent("priorityAgent", priorityAgent, 3)
+    E.add_agent("setupsAgent", setupsAgent, 3)
 
     # Add initial solution
     T = (list(O.orderBook.keys()), O.orderBook)
@@ -201,13 +272,16 @@ def main():
     # Run the evolver
 
     # def (n=1, dom=100, status=100):
-    E.evolve(1000, 100, 100)
+    E.evolve(50000, 100, 100)
 
     collection(E.pop)
 
     # print(f'setups: {setups(T)}')
     # print(f'lowPriority: {lowPriority(T)}')
     # print(f'delay: {delays(T)}')
+
+
+    # swapper(T)
 
 if __name__ == '__main__':
     main()
